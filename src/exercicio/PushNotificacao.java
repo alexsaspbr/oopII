891cf15ab95452acffe8f2d1e965132d1dfd018c
Guilherme <ariel.gmachado@hotmail.com>
2025-08-25 22:58:22 -0300

    CONSEGUIMOS ARRUMAR MDS

---
+++ src/exercicio/PushNotificacao.java
@@ -0,0 +1,9 @@
+package exercicio;
+
+public class PushNotificacao implements Notificacao{
+  @Override
+  public void enviar(String mensagem) {
+    System.out.println("[Push] Enviando: " +  mensagem);
+  }
+}
+
